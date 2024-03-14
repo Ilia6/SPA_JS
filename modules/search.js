0e378d3a37799d2f204dda8decc0b189fac06e6f
Ilia6 <146102840+Ilia6@users.noreply.github.com>
2024-03-14 18:46:46 +0300

    Update search.js

--- modules/search.js
+++ modules/search.js
@@ -1,27 +1,46 @@
-export class Search{
-    constructor(view){
-        this.view = view;
+export class Search {
 
-        this.view.searchInput.addEventListener('keyup', this._debounce(this.searchUsers.bind(this),500));
+    
+
+    constructor(view, log) {
+        this.view = view;
+        this.log = log;
+
+        this.currentUserPage = 0;
+        this.showUserCount = 0;
+
+        this.view.searchInput.addEventListener('keyup', this._debounce(this.searchUsers.bind(this), 500));
     }
 
-  async  searchUsers(){
-    const inpValue = this.view.searchInput.value;
+    setCurrentUserPage(number){
+        this.currentUserPage = number;
+    }
+
+    setShowlUsersCount(number){
+        this.showUserCount = number;
+    }
+
+    async searchUsers() {
+        const inpValue = this.view.searchInput.value;
         return await fetch(`https://api.github.com/search/users?q=${inpValue}`).then(
             (res) => res.json().then(
-                res=>{
+                res => {
                     const users = res.items;
+                    this.setShowlUsersCount(this.showUserCount + user.length);
+                    this.view. showCountMessage(this.log.countMessage(res.total_count));
+                    this.view.toggleViewUserloadMoreBtn(this.showUserCount < res.total_count && this.showUserCount !== res.total_count)
+                    
                     users.forEach(user => this.view.createPrevUser(user));
                 }
             )
-        )
+        );
     }
 
-    _debounce = function(func, wait, immediate) {
+    _debounce(func, wait, immediate) {
         let timeout;
-        return function() {
+        return function () {
             let context = this, args = arguments;
-            let later = function() {
+            let later = function () {
                 timeout = null;
                 if (!immediate) func.apply(context, args);
             };
@@ -30,7 +49,7 @@ export class Search{
             timeout = setTimeout(later, wait);
             if (callNow) func.apply(context, args);
         };
-    };
+    }
 }
 
  
