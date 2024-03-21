f3851c5430b10de1910148f66aa716f8725d7040
Ilia6 <146102840+Ilia6@users.noreply.github.com>
2024-03-21 18:51:50 +0300

    Update api.js

--- modules/api.js
+++ modules/api.js
@@ -1,5 +1,13 @@
+const URL = 'https://api.github.com';
+const USERS_PER_PAGE = 20;
+
+
 export class Api{
     constructor(){
         
+    }
+
+    async searcUsers(inpValue, page){
+        return await fetch(`${URL}/search/users?q=${inpValue}&per_page=${USERS_PER_PAGE}&page=${page}`).then((res) => res.json());
     }
 }
