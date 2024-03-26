74b14e31b446332348217100ce4328b4f62c6ce8
Ilia6 <146102840+Ilia6@users.noreply.github.com>
2024-03-26 18:48:33 +0300

    Update api.js

--- modules/api.js
+++ modules/api.js
@@ -8,6 +8,19 @@ export class Api{
     }
 
     async searcUsers(inpValue, page){
-        return await fetch(`${URL}/search/users?q=${inpValue}&per_page=${USERS_PER_PAGE}&page=${page}`).then((res) => res.json());
+        return await fetch(`${URL}/search/users?q=${inpValue}&per_page=${USERS_PER_PAGE}&page=${page}`).then((res) => res.json())
+    }
+
+    searcUsersData(user){
+        const urls = [
+            `${URL}/users/${user}/followers`,
+            `${URL}/users/${user}/following`,
+            `${URL}/users/${user}/repos`,
+        ];
+
+        const requests = urls.map(url => fetch(url));
+        return Promise.all(requests)
+            .then(responses => Promise.all(responses.map(resp => resp.json())));
+
     }
 }
